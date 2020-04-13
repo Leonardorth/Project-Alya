7f1fab5f79f24e3ad51a61136870bb530d2a93c8
L3ON4RDO <matthawes777@gmail.com>
2020-04-13 22:11:34 +0200

    Hide cursor in gameplay
    Simple hide and lock of cursor on start.

---
+++ Project Alya - 2019.3 Universal RP/Assets/CursorHideAndLock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CursorHideAndLock : MonoBehaviour
+{
+    [SerializeField] bool lockCursor;
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked; //Locks cursor in place
+            Cursor.visible = false;
+        }
+    }
+}
