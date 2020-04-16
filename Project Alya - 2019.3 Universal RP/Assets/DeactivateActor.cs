c0115d53c29665720ee60791e984ee89e1fd316f
L3ON4RDO <matthawes777@gmail.com>
2020-04-16 18:07:02 +0200

    Simple Intro Cutscene
    Added a cutscene at the start of the game with a black fade in and character animation using Unity Timeline and Cinemachine

---
+++ Project Alya - 2019.3 Universal RP/Assets/DeactivateActor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateActor : MonoBehaviour
+{
+    [SerializeField] GameObject animationActor;
+    
+
+    void Start()
+    {
+        animationActor.SetActive(false);
+    }
+}
---
+++ Project Alya - 2019.3 Universal RP/Assets/IntroCutsceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class IntroCutsceneManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject animationActor;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject ui;
+    [SerializeField] PlayableDirector timeline;
+
+    void Start()
+    {
+        player.SetActive(false);
+        animationActor.SetActive(true);
+        ui.SetActive(false);
+        timeline = GetComponent<PlayableDirector>();
+        timeline.Play();
+    }
+
+
+    
+}
