71b3feda7c6dabae937434b5fa6e94f6338f767f
L3ON4RDO <matthawes777@gmail.com>
2020-04-25 22:44:10 +0200

    Improved cutscene
    Added a camera pan at the start of the cutscene as the screen fades to black. Improved camera switching and transitions.

---
+++ Project Alya - 2019.3 Universal RP/Assets/Scripts/MoveVcam4Focus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveVcam4Focus : MonoBehaviour
+{
+    [SerializeField] int speed;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+    
+}
