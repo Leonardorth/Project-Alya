f272bdabf3774a136c73c83b74a4a8e0f290def2
L3ON4RDO <matthawes777@gmail.com>
2020-06-04 13:53:27 +0200

    Added pushing ability to the player
    The player can now push any objects tagged as "PushableObject" using physics. A raycast checks for objects in front of player once he's close enough.

---
+++ Project Alya - 2019.3 Universal RP/Assets/Scripts/ObjectPushing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPushing : MonoBehaviour
+{
+    RaycastHit hit;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float raycastFrequency;
+    public float force;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine("CheckForPushableObject");
+    }
+    
+
+    IEnumerator CheckForPushableObject()
+    {
+        while (true)
+        {
+            
+            if (Physics.Raycast(transform.position + offset, transform.forward, out hit, 0.5f))
+            {
+                //Debug.Log(hit.collider.name);
+                Debug.DrawRay(transform.position + offset, transform.forward, Color.red, 0.5f);
+                if (hit.collider.CompareTag("PushableObject"))
+                {
+                    ApplyForce();
+                    animator.SetBool("isPushing", true);
+                }
+                
+            }
+            else 
+                if (animator.GetBool("isPushing"))
+                {
+                    animator.SetBool("isPushing", false);
+                }
+
+            yield return new WaitForSeconds(raycastFrequency);
+
+        }
+    }
+
+    void ApplyForce()
+    {
+        if (animator.GetBool("isPushing"))
+        {
+
+            var targetRigidbody = hit.collider.GetComponent<Rigidbody>();
+            if (targetRigidbody != null)
+            {
+                Vector3 direction = hit.transform.position - transform.position;
+                targetRigidbody.AddForceAtPosition(direction.normalized * force, hit.point);
+            }
+
+        }
+        
+    }
+}
---
+++ Project Alya - 2019.3 Universal RP/Assets/Scripts/rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    [SerializeField] float yAxis = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, yAxis, 0 * Time.deltaTime);
+    }
+}
